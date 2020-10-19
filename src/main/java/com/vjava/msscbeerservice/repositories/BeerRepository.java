e4f4de0970264527dce238836b64e784f61f955a
Vasu Neelapala <vasu.yo@gmail.com>
2020-10-19 17:22:36 -0400

    Adding Beer Repo. Closes #6

---
+++ src/main/java/com/vjava/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.vjava.msscbeerservice.repositories;
+
+import com.vjava.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
