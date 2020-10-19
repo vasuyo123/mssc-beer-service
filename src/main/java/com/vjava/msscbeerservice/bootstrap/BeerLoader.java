712d0f9dc3348000060cd5133dc83f9b3b2281a7
Vasu Neelapala <vasu.yo@gmail.com>
2020-10-19 17:44:45 -0400

    Adding Bootstrap to load Beer objects. Closes #7

---
+++ src/main/java/com/vjava/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package com.vjava.msscbeerservice.bootstrap;
+
+import com.vjava.msscbeerservice.domain.Beer;
+import com.vjava.msscbeerservice.repositories.BeerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@Slf4j
+public class BeerLoader implements CommandLineRunner {
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder().beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(3368686868868L)
+                    .price(new BigDecimal(4.95))
+                    .build());
+
+            beerRepository.save(Beer.builder().beerName("Galaxy Cat")
+                    .beerStyle("Pale Ale")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(3368686869968L)
+                    .price(new BigDecimal(3.95))
+                    .build());
+        }
+
+        //log.info("Beers Loaded: "+beerRepository.count());
+    }
+}
